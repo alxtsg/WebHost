b747ccfafbc5b1e2393e60d3f31f3c384ef1d405
Alex Tsang <alextsang@live.com>
2017-10-01 15:58:04 +0800

    Code refactoring
    Replaces the use of EventEmitter with Promise, which is a native JavaScript
    feature. The refactoring should also make the flow easier to follow.

--- index.js
+++ index.js
@@ -10,51 +10,36 @@
 
 const express = require('express');
 
-const EventEmitter = require('events');
 const fs = require('fs');
 const https = require('https');
 const path = require('path');
 
-const EVENT_ERROR_READ_CONFIG_FILE = 'EVENT_ERROR_READ_CONFIG_FILE';
-const EVENT_ERROR_PARSE_CONFIG_FILE = 'EVENT_ERROR_PARSE_CONFIG_FILE';
-const EVENT_ERROR_READ_TLS_CERTIFICATE = 'EVENT_ERROR_READ_TLS_CERTIFICATE';
-const EVENT_ERROR_READ_TLS_PRIVATE_KEY = 'EVENT_ERROR_READ_TLS_PRIVATE_KEY';
-
-const EVENT_READ_CONFIG_FILE = 'EVENT_READ_CONFIG_FILE';
-const EVENT_TLS_CONFIG_READY = 'EVENT_TLS_CONFIG_READY';
-const EVENT_STARTED_HTTPS_SERVER = 'EVENT_STARTED_HTTPS_SERVER';
+const ERROR_READ_CONFIG_FILE = 'Cannot read configuration file.';
+const ERROR_PARSE_CONFIG_FILE = 'Cannot parse configuration file.';
+const ERROR_READ_TLS_CERTIFICATE = 'Cannot read TLS certificate.';
+const ERROR_READ_TLS_PRIVATE_KEY = 'Cannot read TLS private key.';
 
 const CONFIG_FILE_PATH = path.join(
   __dirname,
   'config.json'
 );
 
-const EXIT_CODE_ABNORMAL = 1;
-
-/**
- * WebHost.
- *
- * @extends EventEmitter
- */
-class WebHost extends EventEmitter {
-
-  /**
-   * Creates a WebHost instance.
-   */
-  constructor() {
-    super();
-    this.options = {
-      rootDirectory: null,
-      errorPage: null,
-      tls: null
-    };
-    this.expressApp = express();
+const serverConfig = {
+  rootDirectory: null,
+  errorPage: null,
+  tls: {
+    cert: null,
+    key: null,
+    ciphers: null,
+    ecdhCurve: null,
+    secureProtocol: null,
+    port: null
   }
+};
+const expressApp = express();
 
-  /**
-   * Parses and reads from configuration file.
-   */
-  readConfig() {
+function readConfig() {
+  return new Promise((resolve, reject) => {
     fs.readFile(
       CONFIG_FILE_PATH,
       {
@@ -63,80 +48,62 @@ class WebHost extends EventEmitter {
       (readError, data) => {
         let config = null;
         if (readError !== null) {
-          this.emit(EVENT_ERROR_READ_CONFIG_FILE);
+          reject(new Error(ERROR_READ_CONFIG_FILE));
           return;
         }
         try {
           config = JSON.parse(data);
-          this.options.rootDirectory = config.rootDirectory;
-          this.options.errorPage = config.errorPage;
-          this.emit(EVENT_READ_CONFIG_FILE, config.tls);
+          serverConfig.rootDirectory = config.rootDirectory;
+          serverConfig.errorPage = config.errorPage;
+          serverConfig.tls.ciphers = config.tls.ciphers,
+          serverConfig.tls.ecdhCurve = config.tls.ecdhCurve,
+          serverConfig.tls.secureProtocol = config.tls.secureProtocol,
+          serverConfig.tls.port = config.tls.port
+          fs.readFile(config.tls.cert, (readCertError, cert) => {
+            if (readCertError !== null) {
+              reject(new Error(ERROR_READ_TLS_CERTIFICATE));
+              return;
+            }
+            serverConfig.tls.cert = cert;
+            fs.readFile(config.tls.key, (readKeyError, key) => {
+              if (readKeyError !== null) {
+                reject(new Error(ERROR_READ_TLS_PRIVATE_KEY));
+                return;
+              }
+              serverConfig.tls.key = key;
+              resolve();
+            });
+          });
         } catch (parseError) {
-          this.emit(EVENT_ERROR_PARSE_CONFIG_FILE);
+          reject(new Error(ERROR_PARSE_CONFIG_FILE));
         }
       }
     );
-  }
+  });
+}
 
-  /**
-   * Reads TLS certificate file and private key, and prepare several TLS
-   * configurations.
-   *
-   * @param {Object} config TLS configurations.
-   * @param {String} config.cert Path to TLS certificate.
-   * @param {String} config.key Path to TLS private key.
-   * @param {String} config.ciphers Ciphers to use, separated by colon.
-   * @param {String} config.ecdhCurve The curve used for ECDH key agreement.
-   * @param {String} config.secureProtocol The SSL method to use.
-   * @param {number} config.port HTTPS server listening port.
-   */
-  readTLSConfig(config) {
-    this.options.tls = {
-      cert: null,
-      key: null,
-      ciphers: config.ciphers,
-      ecdhCurve: config.ecdhCurve,
-      secureProtocol: config.secureProtocol,
-      port: config.port
-    };
-    fs.readFile(config.cert, (readCertError, cert) => {
-      if (readCertError !== null) {
-        this.emit(EVENT_ERROR_READ_TLS_CERTIFICATE);
-        return;
-      }
-      this.options.tls.cert = cert;
-      fs.readFile(config.key, (readKeyError, key) => {
-        if (readKeyError !== null) {
-          this.emit(EVENT_ERROR_READ_TLS_PRIVATE_KEY);
-          return;
-        }
-        this.options.tls.key = key;
-        this.emit(EVENT_TLS_CONFIG_READY);
-      });
-    });
-  }
-
-  /**
-   * Starts server.
-   */
-  start() {
+/**
+ * Starts server.
+ */
+function start() {
+  return new Promise((resolve, reject) => {
     // Disable several response headers.
-    this.expressApp.disable('etag');
-    this.expressApp.disable('x-powered-by');
+    expressApp.disable('etag');
+    expressApp.disable('x-powered-by');
     // Serve static files by express-static.
-    this.expressApp.use(express.static(
-      this.options.rootDirectory,
+    expressApp.use(express.static(
+      serverConfig.rootDirectory,
       {
         etag: false
       }
     ));
     // File not found.
-    this.expressApp.use((request, response) => {
+    expressApp.use((request, response) => {
       let readStream = null;
       response.statusCode = 404;
       response.type('text/html');
       readStream = fs.createReadStream(
-        this.options.errorPage,
+        serverConfig.errorPage,
         {
           encoding: 'utf8'
         }
@@ -150,44 +117,27 @@ class WebHost extends EventEmitter {
       readStream.pipe(response);
     });
     // Create HTTPS server.
-    https.createServer(
-      {
-        cert: this.options.tls.cert,
-        key: this.options.tls.key,
-        ciphers: this.options.tls.ciphers,
-        ecdhCurve: this.options.tls.ecdhCurve,
-        secureProtocol: this.options.tls.secureProtocol
-      },
-      this.expressApp
-    ).listen(this.options.tls.port);
-    this.emit(EVENT_STARTED_HTTPS_SERVER);
-  }
+    try {
+      https.createServer(
+        {
+          cert: serverConfig.tls.cert,
+          key: serverConfig.tls.key,
+          ciphers: serverConfig.tls.ciphers,
+          ecdhCurve: serverConfig.tls.ecdhCurve,
+          secureProtocol: serverConfig.tls.secureProtocol
+        },
+        expressApp
+      ).listen(serverConfig.tls.port);
+      console.log('Started HTTPS server.');
+      resolve();
+    } catch (error) {
+      reject(error);
+    }
+  });
 }
 
-const webHost = new WebHost();
-webHost.on(EVENT_ERROR_READ_CONFIG_FILE, () => {
-  console.error('Unable to read configuration file.');
-  process.exit(EXIT_CODE_ABNORMAL);
-})
-.on(EVENT_ERROR_PARSE_CONFIG_FILE, () => {
-  console.error('Unable to parse configuration file.');
-  process.exit(EXIT_CODE_ABNORMAL);
-})
-.on(EVENT_ERROR_READ_TLS_CERTIFICATE, () => {
-  console.error('Unable to read TLS certificate.');
-  process.exit(EXIT_CODE_ABNORMAL);
-})
-.on(EVENT_ERROR_READ_TLS_PRIVATE_KEY, () => {
-  console.error('Unable to read TLS private key.');
-  process.exit(EXIT_CODE_ABNORMAL);
-})
-.on(EVENT_READ_CONFIG_FILE, (config) => {
-  webHost.readTLSConfig(config);
-})
-.on(EVENT_TLS_CONFIG_READY, () => {
-  webHost.start();
-})
-.on(EVENT_STARTED_HTTPS_SERVER, () => {
-  console.log('HTTPS server started.');
-});
-webHost.readConfig();
+readConfig()
+  .then(start)
+  .catch((error) => {
+    console.error(error.message);
+  });
