41f0da55761235d82d1b71f0bda0dad0b1bd6464
Alex Tsang <alextsang@live.com>
2019-06-27 23:22:43 +0900

    Use dotenv to get configuration
    Use dotenv to read and parse the configuration file.

---
+++ config.js
@@ -0,0 +1,59 @@
+/**
+ * Configuration module.
+ *
+ * @author Alex TSANG <alextsang@live.com>
+ *
+ * @license BSD-3-Clause
+ */
+
+const dotenv = require('dotenv');
+
+const path = require('path');
+
+/**
+ * Path of .env file.
+ */
+const envFile = path.join(
+  __dirname,
+  '.env'
+);
+
+const config = {
+  rootDirectory: null,
+  errorPage: null,
+  accessLog: null,
+  port: null
+};
+
+const radix = 10;
+
+/**
+ * Gets absolute path.
+ *
+ * @param {string} filePath Path.
+ *
+ * @returns {string} If the path is an absolute path, the path will be returned.
+ *                   If the specified path is not an absolute path, the absolute
+ *                   path relative to the installation directory will be
+ *                   returned.
+ */
+const getAbsolutePath = (filePath) => {
+  if (path.isAbsolute(filePath)) {
+    return filePath;
+  }
+  return path.join(__dirname, filePath);
+};
+
+const result = dotenv.config({
+  path: envFile
+});
+if (result.error !== undefined) {
+  throw new Error(`Unable to read .env: ${result.error.message}`);
+}
+const envConfig = result.parsed;
+config.rootDirectory = getAbsolutePath(envConfig.ROOT_DIRECTORY);
+config.errorPage = getAbsolutePath(envConfig.ERROR_PAGE);
+config.accessLog = getAbsolutePath(envConfig.ACCESS_LOG);
+config.port = parseInt(envConfig.PORT, radix);
+
+module.exports = config;
--- index.js
+++ index.js
@@ -9,9 +9,10 @@
 'use strict';
 
 const cluster = require('cluster');
-const fs = require('fs');
 const os = require('os');
 const path = require('path');
+
+const config = require('./config.js');
 
 const MessageType = require(path.join(
   __dirname,
@@ -23,20 +24,6 @@ const worker = require(path.join(
 ));
 
 /**
- * Error messages.
- */
-const ERROR_READ_CONFIG_FILE = 'Cannot read configuration file.';
-const ERROR_PARSE_CONFIG_FILE = 'Cannot parse configuration file.';
-
-/**
- * Configuration file path.
- */
-const CONFIG_FILE_PATH = path.join(
-  __dirname,
-  'config.json'
-);
-
-/**
  * Handles the message sent from the worker.
  *
  * @param {Object} message Message object sent from the worker.
@@ -46,57 +33,16 @@ function handleWorkerMessage(message) {
 }
 
 /**
- * Reads configuration file.
- *
- * @async
- *
- * @param {String} path Configuration file path.
- *
- * @returns {Promise} Promise will resolve with the configuration, or reject
- *                    with error.
- */
-async function readConfig(path) {
-  return new Promise((resolve, reject) => {
-    fs.readFile(
-      path,
-      {
-        encoding: 'utf8'
-      },
-      (readError, data) => {
-        if (readError !== null) {
-          reject(new Error(ERROR_READ_CONFIG_FILE));
-          return;
-        }
-        try {
-          const config = JSON.parse(data);
-          resolve(config);
-        } catch (parseError) {
-          reject(new Error(ERROR_PARSE_CONFIG_FILE));
-        }
-      }
-    );
-  });
-}
-
-/**
  * Initializes master.
  */
 async function init() {
-  let config = null;
-  try {
-    config = await readConfig(CONFIG_FILE_PATH);
-  } catch (readConfigError) {
-    console.error(
-      `Unable to read configuration file: ${readConfigError.message}`);
-    return;
-  }
   const workerCount = os.cpus().length;
   console.log(`${workerCount} workers will be started.`);
   for (let i = 0; i < workerCount; i++) {
       const worker = cluster.fork();
       worker.send({
         type: MessageType.START_SERVER,
-        config: config
+        config
       });
   }
   cluster.on('message', (worker, message) => {
