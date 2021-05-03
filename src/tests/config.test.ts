bb07817a80d41759ec8575ea8fa7678769d0b09a
Alex Tsang <alextsang@live.com>
2021-05-04 00:20:45 +0900

    Clean up
    Sometimes the return type of a callback function does not matter.

--- src/tests/config.test.ts
+++ src/tests/config.test.ts
@@ -8,8 +8,8 @@ const PORT: number = 8080;
 const ERROR_PAGE: string = '404.html';
 const ACCESS_LOG: string = path.join('logs', 'access.log');
 
-describe('Configurations module', async (): Promise<void> => {
-  it('can load configurations', async (): Promise<void> => {
+describe('Configurations module', async () => {
+  it('can load configurations', async () => {
     assert.strictEqual(config.rootDirectory, ROOT_DIRECTORY);
     assert.strictEqual(config.port, PORT);
     assert.strictEqual(config.errorPage, ERROR_PAGE);
